c70338fff3aa507d2c10c3a9286f666d1c426b2a
Ilyas <ilyas.dev2021@gmail.com>
2023-08-04 17:49:46 +0500

    made the form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { useState } from "react"
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: true },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "spoon", quantity: 12, packed: false },
-  { id: 2, description: "charger", quantity: 1, packed: false }
+  { id: 3, description: "spoon", quantity: 12, packed: false },
+  { id: 4, description: "charger", quantity: 1, packed: false }
 ]
 
 
@@ -27,33 +29,106 @@ function Logo(){
 }
 
 function Form(){
+  const [description,setDescription]=useState("")
+  const [selection,setSelection]=useState("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    if (!description) return;
+
+    const newItem={description,Selection,packed:false,id:Date.now()}
+
+
+    
+
+    setDescription("")
+    setSelection(1)
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you want for tour 😍 trip?</h3>
-    </div>
+      <select value={selection} onChange={(e)=>setSelection(Number(e.target.value))}>
+        {console.log(selection)}
+        {Array.from({length:20},(_,i)=> i +1).map(num=><option value={num} key={num}>{num}</option>)}
+      </select>
+      <input type="text" placeholder="Item..." value={description} onChange={(e)=>setDescription(e.target.value)}></input>
+      <button>ADD</button>
+    </form>
   )
 
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 function PackingList(){
-
-
-
-
   return (
 
     <div className="list">
       <ul className="list">
-        {initialItems.map(item=><Item item={item} />)}
+        {initialItems.map(item=><Item item={item}  key={item.id} />)}
       </ul>
     </div>
 
   )
 
 }
-
-
 
 
 function Item({item}){
@@ -69,29 +144,10 @@ function Item({item}){
   )
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 function Stats(){
   return(
     <footer className="stats">
       <em>💼 you have X items on your list, and you already packed X (X%)</em>
     </footer>
   )
-
-
-
-
-
-
 }
